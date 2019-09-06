182f7d6f9fc3433ef6b6c41e9a70bfa345b3180a
Robert Zhang <louirobert@gmail.com>
2019-09-06 15:16:59 +0800

    Add JUnit test reporter for karma.

--- karma.conf.js
+++ karma.conf.js
@@ -10,7 +10,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
+      require('karma-junit-reporter'),
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -21,6 +22,10 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
+    junitReporter: {
+      outputFile: 'unit-test-result.xml',
+      useBrowserName: false,
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
