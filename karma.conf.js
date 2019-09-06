b04aae194034f7c4dcc42372079ff3fce53c6781
Robert Zhang <louirobert@gmail.com>
2019-09-06 16:46:54 +0800

    Publish test code coverage result.

--- karma.conf.js
+++ karma.conf.js
@@ -17,8 +17,8 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/web-portal'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      dir: require('path').join(__dirname, './coverage'),
+      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
       fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
