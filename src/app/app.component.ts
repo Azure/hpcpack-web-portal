4d6d5515556088b169d99da69114451ac6919c65
Robert Zhang <louirobert@gmail.com>
2019-10-28 16:53:01 +0800

    Remove chart from left side nav list.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,10 +43,10 @@ export class AppComponent {
       title: 'Jobs',
       icon: 'work',
     },
-    {
-      link: 'charts',
-      title: 'Charts',
-      icon: 'bar_chart',
-    },
+    // {
+    //   link: 'charts',
+    //   title: 'Charts',
+    //   icon: 'bar_chart',
+    // },
   ];
 }
