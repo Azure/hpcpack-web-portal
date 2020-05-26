a2b489db6664c1298472b8e92d8b8b1151e8ee3a
Robert Zhang <louirobert@gmail.com>
2020-05-26 17:22:46 +0800

    Disable Google Analytics.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   API_BASE_PATH: '/hpc',
-  GA_TRACK_ID: 'UA-160643556-1',
+  GA_TRACK_ID: '',
   AI_TRACK_ID: '15996569-847e-4561-b9af-5f255efc5949',
   UPDATE_URL: 'https://azure.github.io/hpcpack-web-portal/',
 };
